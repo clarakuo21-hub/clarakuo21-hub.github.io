8ee8f6320ca92c69cc10d4f3d2e6663226b5ad46
clarakuo21-hub <257431956+clarakuo21-hub@users.noreply.github.com>
2026-04-11 02:27:21 +0000

    chore: update album manifest

--- assets/album/manifest.js
+++ assets/album/manifest.js
@@ -1,1 +1,8 @@
-window.ALBUM_IMAGES = [];
+window.ALBUM_IMAGES = [
+  "assets/album/137412.jpg",
+  "assets/album/137417.png",
+  "assets/album/137423.jpg",
+  "assets/album/137424.jpg",
+  "assets/album/137427.jpg",
+  "assets/album/137428.jpg"
+];
